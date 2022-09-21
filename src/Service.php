874c47fdf7efd001de1ae5a999ea97e066c75d22
白云飞 <876337011@qq.com>
2022-09-21 10:36:27 +0800

    Add files via upload

---
+++ src/Service.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace nickbai\filesystem;
+
+class Service extends \think\Service
+{
+    public function register()
+    {
+        $this->app->bind('filesystem', Filesystem::class);
+    }
+}
